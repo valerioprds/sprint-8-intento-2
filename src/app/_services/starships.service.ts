c7ae9a7eaa472e1e10d0db864c31a464cf08f066
valerioprds <v.paradiso89@gmail.com>
2023-07-17 12:35:24 +0200

    images finally display correctly

--- src/app/_services/starships.service.ts
+++ src/app/_services/starships.service.ts
@@ -3,11 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, filter } from 'rxjs/operators';
 
+
 @Injectable({
   providedIn: 'root',
 })
 export class StarshipService {
   constructor(private http: HttpClient) {}
+  private imageURL = ' https://starwars-visualguide.com/assets/img/starships/';
 
   private apiUrl = 'https://swapi.dev/api/starships';
 
@@ -17,10 +19,16 @@ export class StarshipService {
 
     return this.http.get<any>(path);
   }
+  /* crear otra funcion para display imagern  */
 
-
+  getImagesShips(starshipId: string) {
+    return `${this.imageURL}${starshipId}.jpg`;
+  }
 
   getStarships(): Observable<any> {
     return this.http.get(this.apiUrl);
   }
+
+
+ 
 }
--- src/app/ficha/ficha.component.ts
+++ src/app/ficha/ficha.component.ts
@@ -2,6 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { StarshipService } from 'src/app/_services/starships.service';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
+
+
+
 
 @Component({
   selector: 'app-ficha',
@@ -10,13 +15,47 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FichaComponent implements OnInit {
   starship: any;
+  imagesURL: string = '';
+
+  errorImage: string = 'assets/error-icon-28.png';
 
   constructor(
     private route: ActivatedRoute,
-    private starshipService: StarshipService
+    private starshipService: StarshipService,
+    private http: HttpClient,
+    private sanitizer: DomSanitizer
+
   ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const starshipId = params.get('id')!;
+
+      // Call the service to get the starship details by its ID
+      this.starshipService
+        .getStarshipsDetails(starshipId)
+        .subscribe((starship) => {
+          this.starship = starship;
+          this.imagesURL = this.starshipService.getImagesShips(starshipId);
+
+
+          console.log(this.imagesURL)
+        });
+    });
+  }
+
+  sanitizaImage(imagesURL: string): SafeUrl {
+  return  this.sanitizer.bypassSecurityTrustUrl(imagesURL)
+
+  }
+
+
+  getErrorImage() {
+    this.imagesURL = this.errorImage
+  }
+
+
+  /*   ngOnInit() {
     this.route.paramMap.subscribe((params) => {
       const starshipId = params.get('id')!;
 
@@ -25,7 +64,31 @@ export class FichaComponent implements OnInit {
         .getStarshipsDetails(starshipId)
         .subscribe((starship) => {
           this.starship = starship;
+          this.imagesURL = this.starshipService.getImagesShips(starshipId);
         });
     });
-  }
+
+
+  } */
+
+  /*  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const starshipId = params.get('id')!;
+
+      // Llamar al servicio para obtener los detalles de la nave por su ID
+      this.starshipService
+        .getStarshipsDetails(starshipId)
+        .subscribe((starship) => {
+          this.starship = starship;
+
+          // Realizar solicitud HTTP para obtener la URL de la imagen
+          this.http
+            .get<any>('https://starwars-visualguide.com/assets/img/starships/')
+            .subscribe((data) => {
+              this.imageUrl = data.image_url;
+              console.log(data.image_url)
+            });
+        });
+    });
+  } */
 }
