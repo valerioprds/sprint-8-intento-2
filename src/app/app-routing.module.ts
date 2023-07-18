e9eecbe12c2c46600d7440ce138670df34ae15a5
valerioprds <v.paradiso89@gmail.com>
2023-07-18 15:32:42 +0200

    guards half working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,8 +10,8 @@ import { FichaComponent } from './ficha/ficha.component';
 const routes: Routes = [
     { path: '', component: HomeComponent , canActivate: [AuthGuard] },
     { path: 'account/login', component: LoginComponent },
-    { path: 'account/register', component: RegisterComponent },
-    { path: 'starships', component: StarshipComponent },
+    { path: 'account/register', component: RegisterComponent , canActivate: [AuthGuard] },
+    { path: 'starships', component: StarshipComponent, canActivate: [AuthGuard] },
 
     { path: 'starships/:id', component: FichaComponent },
 
